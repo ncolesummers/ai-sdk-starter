19436f91bc859d0f205998913715f6dafa313158
Nate Summers <nsummers@uidaho.edu>
2025-10-26 16:37:32 -0700

    refactor(console, message, multimodal-input, toolbar): update class names for improved styling consistency

--- components/multimodal-input.tsx
+++ components/multimodal-input.tsx
@@ -285,7 +285,7 @@ function PureMultimodalInput({
           />{" "}
           <Context {...contextProps} />
         </div>
-        <PromptInputToolbar className="!border-top-0 border-t-0! p-0 shadow-none dark:border-0 dark:border-transparent!">
+        <PromptInputToolbar className="border-top-0! border-t-0! p-0 shadow-none dark:border-0 dark:border-transparent!">
           <PromptInputTools className="gap-0 sm:gap-0.5">
             <AttachmentsButton
               fileInputRef={fileInputRef}
